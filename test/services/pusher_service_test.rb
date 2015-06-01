efb99c2978a6a11b857ddbd2ca72c93a7bc12dbf
Joshua Toledo <jtoledo@1up.fm>
2015-06-01 19:20:41 -0400

    updated tests

--- test/services/pusher_service_test.rb
+++ test/services/pusher_service_test.rb
@@ -6,7 +6,7 @@ class PusherServiceTest < ActiveSupport::TestCase
   test 'that handle_order should call pusher on channel sales_channel with event order and the order details' do
     order = Order.new(country: 'foo', first_name: 'bar', last_name: 'baz', quantity: 20)
     called = MiniTest::Mock.new
-    called.expect(:call, nil, ['sales_channel', 'order', {first_name: order.first_name, last_name: order.last_name, quantity: order.quantity, country: order.country}])
+    called.expect(:call, nil, ['sales_channel', 'order', {first_name: order.first_name, last_name: order.last_name, quantity: order.quantity, city: order.city, country: order.country}])
     called.expect(:call, nil, [String, String, Hash])
     Pusher.stub :trigger, called do
       PusherService.handle_order(order)
