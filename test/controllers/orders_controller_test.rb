abbe18330516def58df9f4698030aef6764bfe29
Joshua Toledo <jtoledo@1up.fm>
2015-06-01 21:35:41 -0400

    updated test and some views for new order attributes

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,4 +5,18 @@ class OrdersControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:order)
   end
+  
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:order)
+  end
+  
+  test "should create order" do
+    assert_difference('Order.count') do
+      post :create, order: {first_name: 'foo', last_name: 'bar', street_address: '123 baz st', city: 'ft', state: 'fl', country: 'USA', zip_code: '34523', credit_card_number: 'f234', credit_card_code: '321', credit_card_date: '11/20', quantity: 1}
+    end
+ 
+    assert_redirected_to order_path(assigns(:order))
+  end
 end
